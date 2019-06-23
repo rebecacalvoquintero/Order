69bf45660d9b574e8ec9782c88bfd10bac745650
rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2019-06-23 20:55:19 +0100

    Gets server running and swagger configuration to test the future endpoints.

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
+
+@Module( {
+  imports: [TypeOrmModule.forRoot()],
+  controllers: [],
+  providers: [],
+} )
+export class AppModule {
+  constructor(private readonly connection: Connection) {
+  }
+}
